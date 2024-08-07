df33e64c95578e3ae0f02c830bd5ee183456c510
Salar Haider <salarhaider05@gmail.com>
2024-08-07 11:56:17 +0500

    a

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,12 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> -->
+    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/themify-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/selectFX/css/cs-skin-elastic.css')}}">
+
+    <!-- <link rel="stylesheet" href="{{asset('assets/css/style.css')}}"> -->
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
     <title>Products</title>
 </head>
 
@@ -25,23 +35,23 @@
                                 <hr>
                             </div>
                         </div>
-                        <form class="row g-3" method="POST" action="{{route('login_user')}}">
+                        <form class="row " method="POST" action="{{route('login_user')}}">
                             @csrf
-                            <div class="col-md-12">
+                            <div class="col-md-12 mt-3">
                                 <label for="inputEmail4" class="form-label">Email</label>
                                 <input type="email" name="email" class="form-control" id="inputEmail4">
                             </div>
-                            <div class="col-md-12">
+                            <div class="col-md-12 mt-3">
                                 <label for="inputPassword4" class="form-label">Password</label>
                                 <input type="password" name="password" class="form-control" id="inputPassword4">
                             </div>
 
-                            <div class="col-4">
+                            <div class="col-4 mt-3">
                                 <button type="submit" class="btn btn-primary">Sign In</button>
                             </div>
 
-                            <div class="col-7">
-                                <p>Not a member? <a href="{{ route('register') }}">Login</a></p>
+                            <div class="col-7 mt-3">
+                                <p>Not a member? <a href="{{ route('register') }}">Register</a></p>
                             </div>
                         </form>
                     </div>
@@ -59,12 +69,16 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
         crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
         integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script> -->
+    <script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('vendors/popper.js/dist/umd/popper.min.js')}}"></script>
+    <script src="{{asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,12 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    <!-- <link href="{{ asset('/dist/css/bootstrap.min.css') }}" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    <script src="{{ asset('/dist/js/bootstrap.bundle.min.js')}}"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> -->
+    <link rel="stylesheet" href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/themify-icons/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/selectFX/css/cs-skin-elastic.css')}}">
+
+    <!-- <link rel="stylesheet" href="{{asset('assets/css/style.css')}}"> -->
+
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
     <title>Products</title>
 </head>
 
@@ -29,25 +39,26 @@
                             @csrf
                             <div class="col-12">
                                 <label for="name" class="form-label">Full Name</label>
-                                <input type="text" name="name" class="form-control" id="name"
+                                <input type="text" name="name" class="form-control" required id="name"
                                     placeholder="Full Name">
                             </div>
                             <div class="col-md-12">
                                 <label for="inputEmail4" class="form-label">Email</label>
-                                <input type="email" name="email" class="form-control" id="inputEmail4">
+                                <input type="email" name="email" class="form-control" required id="inputEmail4">
                             </div>
                             <div class="col-md-6">
                                 <label for="inputPassword4" class="form-label">Password</label>
-                                <input type="password" name="password" class="form-control" id="inputPassword4">
+                                <input type="password" name="password" class="form-control" required
+                                    id="inputPassword4">
                             </div>
                             <div class="col-md-6">
                                 <label for="inputPassword4" class="form-label">Confirm Password</label>
-                                <input type="password" name="password_confirmation" class="form-control"
+                                <input type="password" name="password_confirmation" required class="form-control"
                                     id="inputPassword4">
                             </div>
 
                             <div class="col-5">
-                            <button type="submit" class="btn btn-primary">Sign Up</button>
+                                <button type="submit" class="btn btn-primary">Sign Up</button>
                             </div>
 
                             <div class="col-7">
@@ -69,12 +80,17 @@
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    <!-- <script src="{{asset('/dist/js/bootstrap.bundle.min.js')}}"
         integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+    <script src="{{asset('/dist/js/bootstrap.min.js')}}"
         integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script> -->
+
+    <script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('vendors/popper.js/dist/umd/popper.min.js')}}"></script>
+    <script src="{{asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 
 </html>
